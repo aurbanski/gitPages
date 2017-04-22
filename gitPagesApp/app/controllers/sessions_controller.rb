0b365508484609ce6792412ec9677f033f88f8c4
danvn <dan.nguyen7@yahoo.com>
2017-04-22 14:16:00 -0600

    OAuth up

--- gitPagesApp/app/controllers/sessions_controller.rb
+++ gitPagesApp/app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      render 'new'
+    user = User.from_omniauth(env["omniauth.auth"])
+
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to request.env['omniauth.origin']
     end
   end
 
   def destroy
+    reset_session
+    redirect_to request.referer
   end
-
 end
