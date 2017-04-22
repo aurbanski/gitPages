cbd97cfb5a9b3b7c52f8edcc40a4aab68c5c99e9
aurbanski <aurbanski27@gmail.com>
2017-04-22 13:16:10 -0600

    Added sesssions

--- gitPagesApp/config/routes.rb
+++ gitPagesApp/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'static_pages/home'
-  root to: "static_pages#home" 
+  root to: "static_pages#home"
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy' 
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
